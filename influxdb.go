b15cb51acbecf02f279477a5b264813ea47236d7
adinesh10 <45469364+adinesh10@users.noreply.github.com>
2019-02-07 02:36:47 -0500

    Add files via upload

---
+++ influxdb.go
@@ -0,0 +1,6 @@
+// Package influxdb is the root package of InfluxDB,
+// the scalable datastore for metrics, events, and real-time analytics.
+//
+// If you're looking for the Go HTTP client for InfluxDB,
+// see package github.com/adinesh10/influxdb/client/v2.
+package influxdb // import "github.com/adinesh10/influxdb"
